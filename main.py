99cdddfe7978d960e539c4995eeadde99b6ba975
wheatonaaron <wheatonaaron@gmail.com>
2022-08-14 18:10:26 -0500

    Fixed x/y moement bug

--- main.py
+++ main.py
@@ -2,8 +2,31 @@
 import tcod
 
 
-def main():
-    print("Hello World!")
+def main() -> None:
+    screen_width = 80
+    screen_height = 50
+    player_x = int(screen_width / 2)
+    player_y = int(screen_width / 2)
+    tileset = tcod.tileset.load_tilesheet(
+        "dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD
+    )
+
+    with tcod.context.new_terminal(
+        screen_width,
+        screen_height,
+        tileset=tileset,
+        title="Entropy - A Chemical Roguelike",
+        vsync=True,
+    ) as context:
+        root_console = tcod.Console(screen_width, screen_height, order="F")
+        while True:
+            root_console.print(x = player_x, y = player_y, string="@")
+
+            context.present(root_console)
+
+            for event in tcod.event.wait():
+                if event.type == "QUIT":
+                    raise SystemExit()
 
 
 if __name__ == "__main__":
